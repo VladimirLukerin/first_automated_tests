d912af38b6ab5de8fefed7283fdfa474be136bd3
VladimirLukerin <119470306+VladimirLukerin@users.noreply.github.com>
2022-11-30 21:47:07 +0300

    Изменение conftest и requirements

--- conftest.py
+++ conftest.py
@@ -2,7 +2,6 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 @pytest.fixture(scope='function')
 def driver():
     driver = webdriver.Chrome(ChromeDriverManager().install())
